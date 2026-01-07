d072b76eeca27965f9fa7ae35dbb0a971fe09915
María Isabel <mi.perezlison@um.es>
2026-01-07 19:07:17 +0100

    Proyecto enviado

--- proyecto/src/main/java/umu/tds/modelo/FactoriaImportadorGastos.java
+++ proyecto/src/main/java/umu/tds/modelo/FactoriaImportadorGastos.java
@@ -6,7 +6,7 @@ public class FactoriaImportadorGastos {
 	public static AdaptadorImportadorGastos getImportador(File archivo) {
 		String nombre = archivo.getName().toLowerCase();
 		if (nombre.endsWith(".csv")) return new AdaptadorCSV();
-		if (nombre.endsWith(".json")) return new AdaptadorJSON();
+		//if (nombre.endsWith(".json")) return new AdaptadorJSON();
 		//if (nombre.endsWith(".txt")) return new AdaptadorTXT();
 		//...
 		throw new IllegalArgumentException("Formato no soportado: " + nombre);
